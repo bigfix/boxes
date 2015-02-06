8def12df451c98c34dba5aee6f01a779e430d223
gnawhleinad <danielleehwang@gmail.com>
2015-02-05 19:55:33 -0800

    add vmware-esx

--- packer/scripts/common/tools.sh
+++ packer/scripts/common/tools.sh
@@ -7,3 +7,18 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
 	umount /mnt
 	rm -f /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso
 fi
+
+if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
+	if grep -q -i "release 6" /etc/redhat-release ; then
+		yum erase -y fuse
+	fi
+
+	cd /tmp
+	mkdir -p /mnt/cdrom
+	mount -o loop /tmp/linux.iso /mnt/cdrom
+	tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
+	/tmp/vmware-tools-distrib/vmware-install.pl --default
+	rm -f /tmp/linux.iso
+	umount /mnt/cdrom
+	rmdir /mnt/cdrom
+fi
--- packer/scripts/redhat/cleanup.sh
+++ packer/scripts/redhat/cleanup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -eux
 
+if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
+	if grep -q -i "release 6" /etc/redhat-release ; then
+		rm /etc/udev/rules.d/70-persistent-net.rules
+		mkdir /etc/udev/rules.d/70-persistent-net.rules
+		rm /lib/udev/rules.d/75-persistent-net-generator.rules
+	fi
+	rm -rf /dev/.udev/
+	if [ -f /etc/sysconfig/network-scripts/ifcfg-eth0 ] ; then
+		sed -i "/^HWADDR/d" /etc/sysconfig/network-scripts/ifcfg-eth0
+		sed -i "/^UUID/d" /etc/sysconfig/network-scripts/ifcfg-eth0
+	fi
+fi
+
 yum -y clean all
 
 dd if=/dev/zero of=/EMPTY bs=1M
