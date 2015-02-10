0eee8f2fe54196d033ee42bb8521f612c242e1ae
gnawhleinad <danielleehwang@gmail.com>
2015-02-09 17:42:59 -0800

    add posts tasks (hostname, iptables)

---
+++ bigfix/redhat/6/hostname.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eux
+
+if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
+	cat > /home/vagrant/hostname.sh << OHANA_MEANS_FAMILY
+number=\$1
+sed -i "s/^\(HOSTNAME=\)\s*.*$/\1redhat65x64-build-\${number}/" /etc/sysconfig/network
+service network restart
+
+sed -i "s/^\(127\.0\.0\.1\s\)*\(.*\)$/\1redhat65x64-build-\${number} \2/" /etc/hosts
+sed -i "s/^\(\:\:1\s*\)\(.*\)$/\1redhat65x64-build-\${number} \2/" /etc/hosts
+OHANA_MEANS_FAMILY
+	chmod 744 /home/vagrant/hostname.sh
+fi
---
+++ bigfix/redhat/6/iptables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eux
+
+if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
+	cat > /home/vagrant/iptables.sh << OHANA_MEANS_FAMILY
+service iptables stop
+iptables -F
+
+iptables -I INPUT -p tcp -s jenkins.sfolab.ibm.com --dport 22 -j ACCEPT
+iptables -I INPUT -p tcp -s 0.0.0.0/0 --dport 22 -j DROP
+
+service iptables save
+chkconfig iptables on
+service iptables start
+OHANA_MEANS_FAMILY
+	chmod 744 /home/vagrant/iptables.sh
+fi
