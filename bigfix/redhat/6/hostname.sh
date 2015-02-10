14f16d7d8afde23d9a0a5e77cc08fb7e4d9f4998
gnawhleinad <danielleehwang@gmail.com>
2015-02-09 19:24:21 -0800

    0eee8f2fe54196d033ee42bb8521f612c242e1ae: fix hostname edit

--- bigfix/redhat/6/hostname.sh
+++ bigfix/redhat/6/hostname.sh
@@ -6,8 +6,8 @@ number=\$1
 sed -i "s/^\(HOSTNAME=\)\s*.*$/\1redhat65x64-build-\${number}/" /etc/sysconfig/network
 service network restart
 
-sed -i "s/^\(127\.0\.0\.1\s\)*\(.*\)$/\1redhat65x64-build-\${number} \2/" /etc/hosts
-sed -i "s/^\(\:\:1\s*\)\(.*\)$/\1redhat65x64-build-\${number} \2/" /etc/hosts
+sed -i "s/^\(127\.0\.0\.1\s*\)\(.*\)$/\1redhat65x64-build-\${number} \2/" /etc/hosts
+sed -i "s/^\(::1\s*\)\(.*\)$/\1redhat65x64-build-\${number} \2/" /etc/hosts
 OHANA_MEANS_FAMILY
 	chmod 744 /home/vagrant/hostname.sh
 fi
