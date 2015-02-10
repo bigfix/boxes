f517afa21ad8828d68c227be111bab0ec3e52b4e
gnawhleinad <danielleehwang@gmail.com>
2015-02-09 22:17:53 -0800

    use -A

--- bigfix/redhat/6/iptables.sh
+++ bigfix/redhat/6/iptables.sh
@@ -6,8 +6,8 @@ jenkins=$(dig +short jenkins.sfolab.ibm.com)
 service iptables stop
 iptables -F
 
-iptables -I INPUT -p tcp -s $jenkins --dport 22 -j ACCEPT
-iptables -I INPUT -p tcp -s 0.0.0.0/0 --dport 22 -j DROP
+iptables -A INPUT -p tcp -s $jenkins --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j DROP
 
 service iptables save
 chkconfig iptables on
