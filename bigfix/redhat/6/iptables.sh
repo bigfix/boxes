5d5fd5606981f78038da940a47f0bfaf379fda12
gnawhleinad <danielleehwang@gmail.com>
2015-02-18 15:38:30 -0800

    condition vmware-iso in the packer template

--- bigfix/redhat/6/iptables.sh
+++ bigfix/redhat/6/iptables.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -eux
 
-if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
-	cat > /home/vagrant/iptables.sh << OHANA_MEANS_FAMILY
-jenkins=$(dig +short jenkins.sfolab.ibm.com)
+cat > /home/vagrant/iptables.sh << OHANA_MEANS_FAMILY
+jenkins=\$(dig +short jenkins.sfolab.ibm.com)
 service iptables stop
 iptables -F
 
-iptables -A INPUT -p tcp -s $jenkins --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp -s \$jenkins --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j DROP
 
 service iptables save
 chkconfig iptables on
 service iptables start
 OHANA_MEANS_FAMILY
-	chmod 744 /home/vagrant/iptables.sh
-fi
+chmod 744 /home/vagrant/iptables.sh
--- bigfix/redhat/6/openjdk.sh
+++ bigfix/redhat/6/openjdk.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -eux
 
-if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
-	rpm -qa | grep -q java-1.8.0-openjdk || yum install -y java-1.8.0-openjdk
-fi
+rpm -qa | grep -q java-1.8.0-openjdk || yum install -y java-1.8.0-openjdk
