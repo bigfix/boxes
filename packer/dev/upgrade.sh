de6bcbc0879bb55e36de0b35e7c7f0da8e752bcd
gnawhleinad <danielleehwang@gmail.com>
2015-02-18 18:22:22 -0800

    add upgrade for packer

--- packer/dev/upgrade.sh
+++ packer/dev/upgrade.sh
@@ -3,15 +3,45 @@
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . $dir/bump.sh
 
-major_version=$(sed -rn 's/^virtualbox\t([0-9]*\.[0-9]*)\..*$/\1/p' $dir/VERSIONS)
+target=$1
 
-sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/virtualbox.sources.list" \
-	-o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-sudo apt-get install --only-upgrade virtualbox-$(version)
+expected_version=$(get $target)
 
-actual_version=$(vboxmanage --version)
-expected_version=$(get virtualbox)
+if [[ $target -eq "virtualbox" ]]; then
+	actual_version=$(vboxmanage --version)
 
-if [[ "$actual_version" -ne "$expected_version" ]]; then
-	bump virtualbox $actual_version
+	major_version=$(sed -rn 's/^virtualbox\t([0-9]*\.[0-9]*)\..*$/\1/p' $dir/VERSIONS)
+
+	sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/virtualbox.sources.list" \
+		-o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+	sudo apt-get install --only-upgrade virtualbox-$(version)
+
+	if [[ "$actual_version" -ne "$expected_version" ]]; then
+		bump virtualbox $actual_version
+	fi
+elif [[ $target -eq "packer" ]]; then
+	actual_version=""
+	if command -v packer 2>&1 >/dev/null; then
+		action_version=$(packer --version | grep -oE '[0-9\.]*')
+	fi
+
+	latest_version=$(curl -sS https://packer.io/downloads.html | \
+		sed -nr 's#^.*Latest version: ([0-9\.]*).*$#\1#p')
+
+	if [[ -n "$actual_version" || "$latest_version" -ne "$expected_version" ]]; then
+		rm -f /opt/packer/*
+		sudo rm -f /usr/local/bin/packer*
+
+		sudo mkdir -p /opt/packer
+		sudo chown -R vagrant:vagrant /opt/packer
+
+		(cd /opt/packer && \
+		curl -L https://dl.bintray.com/mitchellh/packer/packer_${latest_version}_linux_amd64.zip -o packer.zip && \
+		unzip packer.zip && \
+		rm packer.zip)
+
+		sudo ln -s /opt/packer/packer* /usr/local/bin/
+
+		bump packer $latest_version
+	fi
 fi
