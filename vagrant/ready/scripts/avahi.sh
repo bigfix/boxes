d602011e1a754952d07b96b39c0bc4596e9b3f59
gnawhleinad <danielleehwang@gmail.com>
2015-05-30 19:52:25 -0700

    add OHANA option (avahi, mdns)

---
+++ vagrant/ready/scripts/avahi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eux
+
+interface=$(netstat -ie | grep -B1 "172." | head -n 1 | awk -F '[: ]' '{ print $1 }')
+
+sed -i 's/^#\(domain-name\)\s*.*$/\1=test/' /etc/avahi/avahi-daemon.conf
+sed -i 's/^\(browse-domains\s*.*\)$/#\1/' /etc/avahi/avahi-daemon.conf
+sed -i 's,^#\(allow-interfaces\)\s*.*$,\1='"$interface"',' /etc/avahi/avahi-daemon.conf
+
+cat > /etc/mdns.allow << OHANA_MEANS_FAMILY
+.test.
+.test
+OHANA_MEANS_FAMILY
+
+sed -i 's/^\(hosts:.*\)$/\1 mdns/' /etc/nsswitch.conf
+
+service avahi-daemon restart
