fff14e05161d3c99dba7d99a0d8083b50a9810aa
Daniel Hwang <danielleehwang@gmail.com>
2015-06-02 20:51:16 -0700

    fix #6

--- vagrant/ready/scripts/avahi.sh
+++ vagrant/ready/scripts/avahi.sh
@@ -15,4 +15,12 @@ sed -i 's/^\(hosts:.*\)dns\(.*\)$/\1mdns dns\2/' /etc/nsswitch.conf
 sed -i 's/^\(hosts:.*\)mdns[46]_minimal \(.*\)$/\1\2/' /etc/nsswitch.conf
 sed -i 's/^\(hosts:.*\)\[NOTFOUND=return\] \(.*\)$/\1\2/' /etc/nsswitch.conf
 
+if [[ -f /etc/redhat-release ]]; then
+	if grep -q -i "release 7" /etc/redhat-release ; then
+		online="network-online.target"
+		sed -i 's,^\(Requires=avahi.*\)$,\1\nRequires='"$online"'\nAfter='"$online"',' \
+			/usr/lib/systemd/system/avahi-daemon.service
+	fi
+fi
+
 service avahi-daemon restart
